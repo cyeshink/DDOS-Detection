f4d96e4599d8f4a83657829811b3ce45061b58f1
Nick Spencer <Nick Spencer>
2017-03-02 20:11:41 -0800

    Initial GUI commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileSelectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.messageGeneratedTreeView = new System.Windows.Forms.TreeView();
+            this.messageGeneratedFontSize = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.messageGeneratedFontSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 21.36401F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53.57436F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25.06163F));
+            this.tableLayoutPanel1.Controls.Add(this.filePathTextBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.fileSelectButton, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.messageGeneratedTreeView, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.messageGeneratedFontSize, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 2, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 13.63636F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 45.45454F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(700, 517);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.filePathTextBox.Location = new System.Drawing.Point(152, 73);
+            this.filePathTextBox.Multiline = true;
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.Size = new System.Drawing.Size(369, 34);
+            this.filePathTextBox.TabIndex = 0;
+            this.filePathTextBox.TextChanged += new System.EventHandler(this.filePathTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(252, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter file path";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // fileSelectButton
+            // 
+            this.fileSelectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileSelectButton.Location = new System.Drawing.Point(527, 73);
+            this.fileSelectButton.Name = "fileSelectButton";
+            this.fileSelectButton.Size = new System.Drawing.Size(95, 34);
+            this.fileSelectButton.TabIndex = 2;
+            this.fileSelectButton.Text = "Browse...";
+            this.fileSelectButton.UseVisualStyleBackColor = true;
+            this.fileSelectButton.Click += new System.EventHandler(this.fileSelectButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(216, 181);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(241, 29);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Message Generated";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // messageGeneratedTreeView
+            // 
+            this.messageGeneratedTreeView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.messageGeneratedTreeView.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messageGeneratedTreeView.Location = new System.Drawing.Point(152, 213);
+            this.messageGeneratedTreeView.Name = "messageGeneratedTreeView";
+            this.tableLayoutPanel1.SetRowSpan(this.messageGeneratedTreeView, 2);
+            this.messageGeneratedTreeView.Size = new System.Drawing.Size(369, 301);
+            this.messageGeneratedTreeView.TabIndex = 5;
+            this.messageGeneratedTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.messageGeneratedTreeView_AfterSelect);
+            // 
+            // messageGeneratedFontSize
+            // 
+            this.messageGeneratedFontSize.Location = new System.Drawing.Point(527, 213);
+            this.messageGeneratedFontSize.Name = "messageGeneratedFontSize";
+            this.messageGeneratedFontSize.Size = new System.Drawing.Size(120, 22);
+            this.messageGeneratedFontSize.TabIndex = 7;
+            this.messageGeneratedFontSize.ValueChanged += new System.EventHandler(this.messageGeneratedFontSize_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(527, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Font Size";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 517);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.messageGeneratedFontSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button fileSelectButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TreeView messageGeneratedTreeView;
+        private System.Windows.Forms.NumericUpDown messageGeneratedFontSize;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Stack<TreeNode> DisplayNodes = new Stack<TreeNode>();
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyPress +=
+                new KeyPressEventHandler(Form1_KeyPress);
+            messageGeneratedFontSize.Value = 10;
+            //TreeNode DDOS1_L1 = messageGeneratedTreeView.Nodes.Add("DDOS1 : <Example IP1>");
+            //TreeNode DDOS1_L2 = DDOS1_L1.Nodes.Add("DDOS1 : More Info displayed here...");
+            //TreeNode DDOS2_L1 = messageGeneratedTreeView.Nodes.Add("DDOS2 : <Example IP2>");
+            //TreeNode DDOS2_L2 = DDOS2_L1.Nodes.Add("DDOS2 : More Info displayed here...");
+        }
+
+        void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)//enter key
+            {
+                useFile(filePathTextBox.Text);
+            }
+        }
+
+        private void messageGeneratedTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+        }
+
+        private void fileSelectButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openAnalysisFileDialog = new OpenFileDialog();
+            openAnalysisFileDialog.InitialDirectory = "C:\\";
+            openAnalysisFileDialog.Filter = "txt files (*.txt)|*.txt|DDOS Analysis Files (*.daf)|*.daf|All files (*.*)|*.*";
+            openAnalysisFileDialog.FilterIndex = 2;
+            openAnalysisFileDialog.RestoreDirectory = true;
+
+            if(openAnalysisFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                useFile(openAnalysisFileDialog.FileName);
+            }
+        }
+
+        private void useFile(string filename)
+        {
+            filename = filename.ToLower();
+            var lines = File.ReadLines(filename);
+            int lineNumber = lines.Count();
+            for (int i = 0; i < lineNumber; i++)
+            {
+                if (filename.Contains(".daf"))//parse DAF file if it's daf file
+                {
+                    TreeNode root = displayString(lines.ElementAt(i), null);
+                    if (lineNumber % 2 == 0)
+                    {
+                        TreeNode level1 = displayString(lines.ElementAt(++i), root);
+                    }
+                }
+                //call python analysis method if it's txt file and display it in tiers
+                /*WIP
+                if (filename.Contains(".txt"))
+                {
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "python.exe";
+                    startInfo.Arguments = "-c import ddosanalysis; ddosanalysis.process(filename)";
+                    process.StartInfo = startInfo;
+                    process.Start();
+                    //after program has analyzed log and made .daf file
+                    string newFileName = filename.strip(".txt");
+                    useFile(newFileName);
+                }*/
+
+            }
+            //filePathTextBox.Text = filename;
+        }
+        private TreeNode displayString(string line, TreeNode parent)//parent=null for root
+        {
+            TreeNode newNode;
+            if (parent == null)
+            {
+                newNode = messageGeneratedTreeView.Nodes.Add(line);
+            }
+            else
+            {
+                newNode = parent.Nodes.Add(line);
+            }
+            return newNode;
+        }
+
+        private void filePathTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void messageGeneratedFontSize_ValueChanged(object sender, EventArgs e)
+        {
+            messageGeneratedTreeView.Font = new Font(messageGeneratedTreeView.Font.FontFamily, (Single)messageGeneratedFontSize.Value);
+        }
+    }
+
+}
+
