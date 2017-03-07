d2679966ae6988247fcc3f5bb67780663228000a
cyeshink <cyeshink@gmail.com>
2017-03-07 14:36:25 -0800

    Add files via upload

--- rbddos.py
+++ rbddos.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 import sys
+import os
 
 for arg in sys.argv:
     log = arg
-file = open(arg, 'r') 
+file = open(arg, 'r')
+if os.stat(log).st_size==0:
+    print 'Incorrect Log Format or Empty Log'
+    sys.exit(0)
 daf = open('report.daf', 'w')
 cnt = 0
 prevtime = 0
+check = 0
+llist = []
 userfriendlyoutput = []
 detailedoutput     = []
 for curlog in file.readlines():
+    if check == 0:
+        llist = curlog.split(' ')
+        if not llist:
+            print 'Incorrect Log Format'
+            sys.exit(0)
+        elif len(llist) != 9:
+            print 'Incorrect Log Format'
+            sys.exit(0)
+        else:
+            check == 1
+
     logn, date, time, etc, protocol, x, port, ipin, ipout = curlog.split()
     if prevtime == time:
         cnt += 1
@@ -26,5 +43,3 @@ for i in range(len(userfriendlyoutput)):
     print >> daf, "ddos #" + str(i+1)
     print >> daf, "IP address: " + userfriendlyoutput[i]
     print >> daf, detailedoutput[i]
-    
-
